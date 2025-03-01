cd748263d48fb9dbe6c2ceec658e6c3ca0fffa9c
tagayalexandra <Tagayaleksandra@gmail.com>
2025-03-01 16:05:57 +0300

    implemented functionality of WorkSpaceService;

--- src/main/java/org/example/service/impl/WorkSpaceService.java
+++ src/main/java/org/example/service/impl/WorkSpaceService.java
@@ -1,29 +1,39 @@
 package org.example.service.impl;
 
 import org.example.entity.WorkSpace;
+import org.example.repository.Repository;
 import org.example.service.Service;
 
 public class WorkSpaceService implements Service<WorkSpace> {
 
+    private final Repository<WorkSpace> repository;
+
+    public WorkSpaceService(Repository<WorkSpace> repository) {
+        this.repository = repository;
+    }
+
     @Override
     public void create(WorkSpace workSpace) {
+        repository.create(workSpace);
 
 
     }
 
     @Override
     public WorkSpace read(Long id) {
-        return null;
+        return repository.read(id);
     }
 
     @Override
     public void update(WorkSpace workSpace) {
+        repository.update(workSpace);
 
 
     }
 
     @Override
     public void delete(Long id) {
+        repository.delete(id);
 
     }
 }
