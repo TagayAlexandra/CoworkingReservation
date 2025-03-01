e69b7dd6522229b00cb8321819421f5dd62db8e2
tagayalexandra <Tagayaleksandra@gmail.com>
2025-03-01 15:54:58 +0300

    implemented functionality of CollectionWorkSpaceRepository;

--- src/main/java/org/example/repository/impl/CollectionWorkSpaceRepository.java
+++ src/main/java/org/example/repository/impl/CollectionWorkSpaceRepository.java
@@ -3,27 +3,49 @@ package org.example.repository.impl;
 import org.example.entity.WorkSpace;
 import org.example.repository.WorkSpaceRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 
 public class CollectionWorkSpaceRepository implements WorkSpaceRepository {
 
 
+    private final List<WorkSpace> workSpaceList = new ArrayList<>();
+
+
     @Override
     public void create(WorkSpace workSpace) {
+        workSpaceList.add(workSpace);
 
     }
 
     @Override
     public WorkSpace read(Long id) {
-        return null;
+        int index = getIndexById(id).orElseThrow();
+        return workSpaceList.get(index);
     }
 
     @Override
     public void update(WorkSpace workSpace) {
+        int index = getIndexById(workSpace.getId()).orElseThrow();
+        workSpaceList.set(index, workSpace);
 
     }
 
     @Override
     public void delete(Long id) {
+        int index = getIndexById(id).orElseThrow();
+        workSpaceList.remove(index);
 
+    }
+
+    private Optional<Integer> getIndexById(Long id) {
+        for (int i = 0; i < workSpaceList.size(); i++) {
+            if (workSpaceList.get(i).getId().equals(id)) {
+                return Optional.of(i);
+            }
+        }
+        return Optional.empty();
     }
 }
