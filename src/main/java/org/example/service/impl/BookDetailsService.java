62f69f701f2b8af9fde4ce4a8d18cb9887b388b8
tagayalexandra <Tagayaleksandra@gmail.com>
2025-03-01 15:55:43 +0300

    implemented functionality of BookDetailsService;

--- src/main/java/org/example/service/impl/BookDetailsService.java
+++ src/main/java/org/example/service/impl/BookDetailsService.java
@@ -1,28 +1,38 @@
 package org.example.service.impl;
 
 import org.example.entity.BookingDetail;
+import org.example.repository.Repository;
 import org.example.service.Service;
 
 public class BookDetailsService implements Service<BookingDetail> {
 
+    private final Repository<BookingDetail> repository;
+
+    public BookDetailsService(Repository<BookingDetail> repository) {
+        this.repository = repository;
+    }
+
     @Override
     public void create(BookingDetail bookingDetail) {
+        repository.create(bookingDetail);
 
     }
 
     @Override
     public BookingDetail read(Long id) {
-        return null;
+        return repository.read(id);
     }
 
     @Override
     public void update(BookingDetail bookingDetail) {
+        repository.update(bookingDetail);
 
 
     }
 
     @Override
     public void delete(Long id) {
+        repository.delete(id);
 
     }
 }
