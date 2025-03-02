6c86ee401b321bb008e08ab4fb6c842f0cd08d19
tagayalexandra <Tagayaleksandra@gmail.com>
2025-03-02 17:48:55 +0300

    implemented method view in classes AdminMenu, MainMenu , UserMenu;

--- src/main/java/org/example/view/impl/UserMenu.java
+++ src/main/java/org/example/view/impl/UserMenu.java
@@ -1,10 +1,18 @@
 package org.example.view.impl;
 
 import org.example.view.Menu;
+import org.example.view.MessageConstant;
+
 
 public class UserMenu implements Menu {
+
     @Override
     public void view() {
+        System.out.println(MessageConstant.USER_ACTION_PROMPT);
+        System.out.println(MessageConstant.SELECTED_VALUE);
+
 
     }
+
+
 }
